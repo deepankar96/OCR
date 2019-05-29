a5d95f976613a8eb6620518b23f7e0ad0ed63133
deepankar96 <34167524+deepankar96@users.noreply.github.com>
2019-05-29 20:21:14 +0530

    Add files via upload

--- objectDetection.py
+++ objectDetection.py
@@ -6,39 +6,39 @@ Created on Sun May 26 10:59:14 2019
 """
 
 import cv2
+import matplotlib as plt
+import numpy as np
 
-image = cv2.imread('Test_image_bnw1.png')
-img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAYSCALE)
+initial_image = cv2.imread('IMG_20190528_212944.jpg')
+gray_image = cv2.cvtColor(initial_image, cv2.COLOR_BGR2GRAY)
+median_image = cv2.medianBlur(gray_image, 3)
 
 
-height = len(img)
+height = len(median_image)
+flag_for_separation=0
+line_index = []
+j=0
+lines = []
 
-flag=0
-start = []
-stop = []
-
-final_img = []
-
+#plt.pyplot.imshow(sharpened)
 
 for i in range(height):
-    if img[i][10]==255 and flag==0:
-        start.append(i)
-        flag=1
-        continue
-    if img[i][10]==255 and flag==1:
-        stop.append(i)
-        flag=0
-        continue
+    if median_image[i][10]==0 and flag_for_separation==0:
+        line_index.append(i)
+        flag_for_separation=1
+    if flag_for_separation==1:
+        j+=1
+        if j==6:
+            j=0
+            flag_for_separation=0
         
 
-for i in range(len(stop)):
-    print(start[i])
-    print(stop[i])
-    print()
-'''
-  r1 = stop[1]-start[1]
-  
-for i in range(r1):
-    for j in range(2843):
-        final_img.append(r1,j)
-print(final_img)'''
+for i in range(len(line_index)-1):
+    start=line_index[i]
+    stop=line_index[i+1]
+    temp=[]
+    for j in range(start,stop):
+        temp.append(median_image[j])
+    lines.append(temp)
+    
+plt.pyplot.imshow(lines[2])    
